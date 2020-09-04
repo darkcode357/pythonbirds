0e066e570c07d4223831b756edc57af14b25117b
darkcode0x00 <darkcode357@gmail.com>
2020-09-04 12:38:13 -0300

    criado o atributo idade

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,8 +1,11 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):
+        self.idade = idade
+        self.nome = nome
     def comprimentar(self):
         return f'olá{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("darkcode")
     print(id(p))
-    print(p.comprimentar(3,))
+    print(p.nome)
